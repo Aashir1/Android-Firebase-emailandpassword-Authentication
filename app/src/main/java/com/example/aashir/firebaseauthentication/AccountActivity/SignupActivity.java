028adfdd5e7eb3bccd62067da2da30ee2419a8d9
Aashir <kaashir9@gmail.com>
2018-04-19 08:43:05 +0500

    Registration and signIn, signOut

---
+++ app/src/main/java/com/example/aashir/firebaseauthentication/AccountActivity/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.example.aashir.firebaseauthentication.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aashir.firebaseauthentication.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
